7b84f230a357cd730daf4aac168e4959c49a5b06
Vinod Akkepalli <shining.vinod@gmail.com>
2026-05-28 16:07:21 +0530

    Time: 1 ms (99.87%) | Memory: 55.5 MB (66.04%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 1;
+        int val = nums[0];
+
+        for(int i = 1; i < nums.length; i++) {
+            if( nums[i] == val ) {
+                count++;
+            } else if( nums[i] != val ) {
+                if(count == 0) {
+                    val = nums[i];
+                } else {
+                    count--;
+                }
+            }
+        }
+        return val;
+    }
+}
