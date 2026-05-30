1efab15e64743bc23646ac4ce6a9d1bf4d3fb445
Vinod Akkepalli <shining.vinod@gmail.com>
2026-05-30 08:46:13 +0530

    Time: 1 ms (100.00%) | Memory: 46.8 MB (70.76%) - LeetSync

--- 674-longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.java
+++ 674-longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.java
@@ -3,14 +3,10 @@ class Solution {
         int ans=1;
         int maxLen =1;
 
-        if(nums.length == 1) {
-            return 1;
-        }
-
         for(int i=1; i< nums.length; i++) {
             if (nums[i] > nums[i-1]) {
                 ans++;
-            } else if(nums[i] <= nums[i-1]) {
+            } else {
                 ans = 1;
             }
             maxLen = Math.max(maxLen, ans);
